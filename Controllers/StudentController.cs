572c1c98e6221bf5a5aa045ea88085b5f6790bda
Rahul Yadav <72229574+rahulyadav111@users.noreply.github.com>
2022-09-14 17:33:04 +0530

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BindDropDownMVC.Models;
+
+namespace BindDropDownMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+
+            Student objuser = new Student();
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Rahul; Persist Security Info=True; User ID=sa; Password=Sa@2014"))
+            {
+                using (SqlCommand cmd = new SqlCommand("select * from userdetails", con))
+                {
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                    List<Student> userlist = new List<Student>();
+                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        Student uobj = new Student();
+                        uobj.UserId = Convert.ToInt32(ds.Tables[0].Rows[i]["userid"].ToString());
+                        uobj.Education = ds.Tables[0].Rows[i]["education"].ToString();
+                        uobj.UserName = ds.Tables[0].Rows[i]["username"].ToString();
+                        uobj.Education = ds.Tables[0].Rows[i]["education"].ToString();
+                        uobj.Location = ds.Tables[0].Rows[i]["location"].ToString();
+                        userlist.Add(uobj);
+                    }
+                    objuser.usersinfo = userlist;
+                }
+                con.Close();
+            }
+            return View(objuser);
+        }
+    }
+}
+
+       
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BindDropDownMVC.Models
+{
+    public class Student
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Education { get; set; }
+        public string Location { get; set; }
+        public List<Student> usersinfo { get; set; }
+    }
+}
